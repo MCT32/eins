86635f6f7413bc2a2557d286f07ca5d911784a94
MCT32 <ivanhen99@gmail.com>
2025-04-24 00:15:05 +1000

    ratatui setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use color_eyre::Result;
+use crossterm::event::{self, Event, KeyCode, KeyEvent, KeyEventKind};
+use ratatui::{DefaultTerminal, Frame};
+
+fn main() -> Result<()> {
+    color_eyre::install().unwrap();
+    let terminal = ratatui::init();
+    let result = run(terminal);
+    ratatui::restore();
+    result
+}
+
+fn run(mut terminal: DefaultTerminal) -> Result<()> {
+    loop {
+        terminal.draw(render).unwrap();
+        if matches!(event::read().unwrap(), Event::Key(KeyEvent {
+            code: KeyCode::Esc,
+            modifiers: _,
+            kind: KeyEventKind::Press,
+            state: _
+        })) {
+            break Ok(());
+        }
+    }
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
 }
